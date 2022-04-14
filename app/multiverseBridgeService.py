6e0c90cec22390be9f3b7f3042afec625715ad4e
Shawn Casey <shawn.casey@verasci.com>
2022-04-14 17:37:15 -0400

    Update api_url for multiverse bridge

--- app/multiverseBridgeService.py
+++ app/multiverseBridgeService.py
@@ -4,7 +4,7 @@ from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 
 
-api_url = "https://mvb-api-dev.herokuapp.com/api/v1/cards/search?"
+api_url = "multiversebridge.com/api/v1/cards/search?"
 retry_strategy = Retry(
     total=5,
     status_forcelist=[429, 500, 502, 503, 504],
